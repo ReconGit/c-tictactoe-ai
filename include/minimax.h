ba6da32ef4235ab2641f81ce3205dc4b0c7a6171
Jakub Gerec <jakub.gerec@student.tuke.sk>
2023-03-17 23:57:43 +0100

    changed function description in header file

--- include/minimax.h
+++ include/minimax.h
@@ -1,4 +1,4 @@
 
-// Returns the best or one of the best possible moves using the minmax algorithm
-// note: no depth limit
+// Returns the first best possible move using the minimax algorithm
+// note: no depth limit, branching until solved 
 struct Move find_best_move(char board[3][3], char player);
