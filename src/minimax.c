3655afd93e5d3c623a20c7495a01afb0ecab4a02
Jakub Gerec <jakub.gerec@student.tuke.sk>
2023-03-19 02:23:01 +0100

    added iterative deepening, more tests

--- src/minimax.c
+++ src/minimax.c
@@ -7,17 +7,17 @@
 #define MIN(a,b) (((a)<(b)) ? (a) : (b))
 #define MAX(a,b) (((a)>(b)) ? (a) : (b))
 
-static int minimax(char board[3][3], char ai_player, bool ai_turn, int alpha, int beta);
+static int minimax(char board[3][3], char ai_player, int depth, bool ai_turn, int alpha, int beta);
 
 struct Move find_best_move(char board[3][3], char ai_player)
 {
-    int best_value = -9999;
+    int best_value = INT_MIN;
     struct Move best_move = {-1, -1};
     for (int y = 0; y < 3; y++) {  // branch each valid move
         for (int x = 0; x < 3; x++) {
             if (board[y][x] == ' ') {
                 board[y][x] = ai_player;  // try move
-                int move_value = minimax(board, ai_player, false, INT_MIN, INT_MAX);
+                int move_value = minimax(board, ai_player, 9, false, INT_MIN, INT_MAX);
                 board[y][x] = ' ';  // undo move
 
                 if (move_value > best_value) {
@@ -31,16 +31,15 @@ struct Move find_best_move(char board[3][3], char ai_player)
     return best_move;
 }
 
-static int minimax(char board[3][3], char ai_player, bool ai_turn, int alpha, int beta)
+static int minimax(char board[3][3], char ai_player, int depth, bool ai_turn, int alpha, int beta)
 {
-    char opponent = (ai_player == 'X') ? 'O' : 'X';
-
+    // win as soon as possible, lose as late as possible
     enum Winner winner = check_winner(board);
     if (winner == X_WIN) {
-        return (ai_player == 'X') ? 10 : -10;
+        return (ai_player == 'X') ? 10 + depth : -10 - depth;
     } else if (winner == O_WIN) {
-        return (ai_player == 'O') ? 10 : -10;
-    } else if (winner == DRAW) {
+        return (ai_player == 'O') ? 10 + depth : -10 - depth;
+    } else if (winner == DRAW || depth == 0) {
         return 0;
     }
 
@@ -51,7 +50,7 @@ static int minimax(char board[3][3], char ai_player, bool ai_turn, int alpha, in
             for (int x = 0; x < 3; x++) {
                 if (board[y][x] == ' ') {
                     board[y][x] = ai_player;
-                    value = MAX(value, minimax(board, ai_player, false, alpha, beta));
+                    value = MAX(value, minimax(board, ai_player, depth - 1, false, alpha, beta));
                     board[y][x] = ' ';
 
                     alpha = MAX(alpha, value);
@@ -62,12 +61,13 @@ static int minimax(char board[3][3], char ai_player, bool ai_turn, int alpha, in
             }
         }
     } else {
+        char opponent = (ai_player == 'X') ? 'O' : 'X';
         value = INT_MAX;
         for (int y = 0; y < 3; y++) {
             for (int x = 0; x < 3; x++) {
                 if (board[y][x] == ' ') {
                     board[y][x] = opponent;
-                    value = MIN(value, minimax(board, ai_player, true, alpha, beta));
+                    value = MIN(value, minimax(board, ai_player, depth - 1, true, alpha, beta));
                     board[y][x] = ' ';
 
                     beta = MIN(beta, value);
--- tests/tests.c
+++ tests/tests.c
@@ -42,14 +42,41 @@ TEST test_check_winner()
 
 TEST test_find_best_move()
 {
-    char board[3][3] = {
-        {' ', ' ', 'X'},
+    char board1[3][3] = {
+        {'X', 'X', ' '},
+        {' ', 'O', 'O'},
+        {' ', ' ', ' '}
+    };
+    struct Move best_move = find_best_move(board1, 'X');
+    ASSERT_EQ_FMTm("1.x coordinate", 2, best_move.x, "%d");
+    ASSERT_EQ_FMTm("1.y coordinate", 0, best_move.y, "%d");
+    best_move = find_best_move(board1, 'O');
+    ASSERT_EQ_FMTm("2.x coordinate", 0, best_move.x, "%d");
+    ASSERT_EQ_FMTm("2.y coordinate", 1, best_move.y, "%d");
+
+    char board2[3][3] = {
+        {'O', 'O', ' '},
+        {' ', 'X', 'X'},
+        {' ', ' ', ' '}
+    };
+    best_move = find_best_move(board2, 'X');
+    ASSERT_EQ_FMTm("3.x coordinate", 0, best_move.x, "%d");
+    ASSERT_EQ_FMTm("3.y coordinate", 1, best_move.y, "%d");
+    best_move = find_best_move(board2, 'O');
+    ASSERT_EQ_FMTm("4.x coordinate", 2, best_move.x, "%d");
+    ASSERT_EQ_FMTm("4.y coordinate", 0, best_move.y, "%d");
+
+    char board3[3][3] = {
+        {'O', ' ', 'X'},
         {' ', 'X', 'O'},
         {' ', ' ', 'O'}
     };
-    struct Move best_move = find_best_move(board, 'O');
-    ASSERT_EQ_FMTm("x coordinate", 0, best_move.x, "%d");
-    ASSERT_EQ_FMTm("y coordinate", 2, best_move.y, "%d");
+    best_move = find_best_move(board3, 'X');
+    ASSERT_EQ_FMTm("5.x coordinate", 0, best_move.x, "%d");
+    ASSERT_EQ_FMTm("5.y coordinate", 2, best_move.y, "%d");
+    best_move = find_best_move(board3, 'O');
+    ASSERT_EQ_FMTm("6.x coordinate", 0, best_move.x, "%d");
+    ASSERT_EQ_FMTm("6.y coordinate", 2, best_move.y, "%d");
     PASS();
 }
 
