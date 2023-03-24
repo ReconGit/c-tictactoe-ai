48474f6c618fdc6aea08f6a7e299418c02435ac4
Jakub Gerec <jakub.gerec@student.tuke.sk>
2023-03-24 20:13:49 +0100

    mcts

--- src/mcts.c
+++ src/mcts.c
@@ -35,12 +35,12 @@ struct Node {
     char current_player;
     int children_count;
     int visit_count;
-    double total_score;
+    double value;
 };
 
-static double UCT(struct Node* node, double exploration_param);
+static double UCT(struct Node* node);
 static struct Node* select(struct Node* root);
-static struct Node* expand(struct Node *node, int move_idx);
+static struct Node* expand(struct Node *node);
 static int simulate(struct Node *node, char ai_player);
 static void backpropagate(struct Node *node, double reward);
 static struct Move choose_best_move(struct Node *root);
@@ -48,13 +48,14 @@ static void free_tree(struct Node *node);
 
 struct Move mcts_find_best_move(char board[3][3], char ai_player)
 {
+    srand(time(NULL));
     // initialize the root node
     struct Node* root = (struct Node*)malloc(sizeof(struct Node));
     memcpy(root->board, board, sizeof(root->board));
     root->current_player = (ai_player == 'X') ? 'O' : 'X';  // set the opposite at root
     root->children_count = get_valid_moves(root->board, root->moves);
     root->visit_count = 0;
-    root->total_score = 0.0;
+    root->value = 0.0;
     root->current_move = (struct Move){-1, -1};
     root->parent = NULL;
     root->children = (struct Node**)malloc(root->children_count * sizeof(struct Node*));
@@ -63,20 +64,21 @@ struct Move mcts_find_best_move(char board[3][3], char ai_player)
     }
 
     // perform MCTS in limited time (1 sec)
-    time_t start_time = time(NULL);
-    while (time(NULL) - start_time < 1) {
+    //time_t start_time = time(NULL);
+    // while (time(NULL) - start_time < 1) {
+    int max_iterations = 1000;
+    for (int i = 0; i < max_iterations; i++) {
         struct Node* node = select(root);
-        if (node == NULL) {
-            break;  // explored till end
-        }
+        struct Node* child = expand(node);
+        if (child == NULL) continue;
         //printf("Simulating... x:%d,y:%d p:%c\n", child->current_move.x, child->current_move.y, child->current_player);
-        double score = simulate(node, ai_player);
+        double score = simulate(child, ai_player);
         //printf("Score: %f\n\n", score);
         backpropagate(node, score);
     }
 
     // for (int i = 0; i < root->children_count; i++) {
-    //     printf("Move: %d %d, score: %f, visit count: %d\n", root->moves[i].x, root->moves[i].y, root->children[i]->total_score, root->children[i]->visit_count);
+    //     printf("Move: %d %d, score: %f, visit count: %d\n", root->moves[i].x, root->moves[i].y, root->children[i]->value, root->children[i]->visit_count);
     // }
     // print_board(root->board);
     // printf("MCTS: %d simulations\n", root->visit_count);
@@ -91,19 +93,16 @@ struct Move mcts_find_best_move(char board[3][3], char ai_player)
 static struct Node* select(struct Node* root)
 {
     struct Node* node = root;
-    //printf("1\n");
-    while (node->visit_count > 0 && node->children_count > 0) {
+    while (check_winner(node->board) == PLAYING) {
         double best_uct = -INFINITY;
-        //printf("2\n");
-        struct Node* best_child = NULL;
+        struct Node* best_child = node->children[0];
 
         for (int i = 0; i < node->children_count; i++) {
             if (node->children[i] == NULL) {
-                //printf("3\n");
-                return expand(node, i);
+                return node;
             }
             struct Node* child = node->children[i];
-            double uct = UCT(child, UCT_CONSTANT);
+            double uct = UCT(child);
             //printf("UCT: %f move: x:%d, y:%d p:%c\n", uct, child->current_move.x, child->current_move.y, child->current_player);
             if (uct > best_uct) {
                 best_uct = uct;
@@ -112,30 +111,33 @@ static struct Node* select(struct Node* root)
         }
         node = best_child;
     }
-    //printf("4\n");
     return node;
 }
 
-static struct Node *expand(struct Node *node, int move_idx)
+static struct Node *expand(struct Node *node)
 {
-    if (check_winner(node->board) != PLAYING) return NULL;  // game over (no more moves)
+    if (node->children_count == 0) return NULL;  // game over (no more moves)
 
-    node->children[move_idx] = (struct Node*)malloc(sizeof(struct Node));
+    int i = 0;
+    while (node->children[i] != NULL) {
+        i++;
+    }
+    node->children[i] = (struct Node*)malloc(sizeof(struct Node));
 
-    struct Node* child = node->children[move_idx];
+    struct Node* child = node->children[i];
     memcpy(child->board, node->board, sizeof(node->board));
     child->current_player = (node->current_player == 'X') ? 'O' : 'X';  // set the opposite of previous
-    child->current_move = node->moves[move_idx];
+    child->current_move = node->moves[rand() % node->children_count];
     child->board[child->current_move.y][child->current_move.x] = child->current_player;  // simulate move
     child->children_count = get_valid_moves(child->board, child->moves);
     child->visit_count = 0;
-    child->total_score = 0.0;
+    child->value = 0.0;
     child->parent = node;
     child->children = (struct Node**)malloc(child->children_count * sizeof(struct Node*));
     for (int i = 0; i < child->children_count; i++) {
         child->children[i] = NULL;
     }
-    node->children[move_idx] = child;
+    //node->children[i] = child;
 
     return child;
 }
@@ -172,7 +174,7 @@ static void backpropagate(struct Node *node, double reward)
     // update the total reward and visit count for the current node
     while (node != NULL) {
         node->visit_count++;
-        node->total_score += reward;
+        node->value += reward;
         node = node->parent;
     }
 }
@@ -185,9 +187,10 @@ static struct Move choose_best_move(struct Node *root)
 
         struct Node* child = root->children[i];
         if (child != NULL) {
-            if (child->total_score > best_score) {
-                best_move = root->moves[i];
-                best_score = child->total_score;
+            double score = child->value / child->visit_count;
+            if (score > best_score) {
+                best_move = child->current_move;
+                best_score = score;
             }
         } else {
             break;
@@ -196,12 +199,12 @@ static struct Move choose_best_move(struct Node *root)
     return best_move;
 }
 
-static double UCT(struct Node* node, double exploration_param)
+static double UCT(struct Node* node)
 {
-    if (node->visit_count == 0) return INFINITY; // node is not expanded yet
+    //if (node->visit_count == 0) return INFINITY; // node is not expanded yet
 
-    double exploitation = node->total_score / node->visit_count;
-    double exploration = exploration_param * sqrt(log(node->parent->visit_count) / node->visit_count);
+    double exploitation = node->value / node->visit_count;
+    double exploration = UCT_CONSTANT * sqrt(log(node->parent->visit_count) / node->visit_count);
     return exploitation + exploration;
 }
 
